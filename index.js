87a44545c648c85c8cd54e343fe6fbfe41cc1c5c
thuytrang003 <truongnguyenthuytrang2003@gmail.com>
2024-04-13 09:57:19 +0700

    last

--- index.js
+++ index.js
@@ -6,5 +6,4 @@ import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 import thuyTrang from './thuyTrang';
-import StopWatch from './stopWatch';
 AppRegistry.registerComponent(appName, () => thuyTrang);
